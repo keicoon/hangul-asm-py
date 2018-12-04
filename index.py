294fed6467dc92509dce6f56ca277b830c9bd099
keicoon <keicoon15@gmail.com>
2018-12-04 14:53:21 +0900

    convert script to python style

--- index.py
+++ index.py
@@ -1,123 +1,83 @@
 
-FIRST_KORAEN_CHAR = "\uac00
+FIRST_KORAEN_CHAR = "\uac00"
 LAST_KOREAN_CHAR = "\ud7af"
 KOREAN_UNICODE_DEFAULT_INDEX = 0xac00
 
 DEFAULT_IMT_ARR = (19, 21, 28)
 USING_CHARCODE = False
 DEFAULT_ASCII_INDEX = 128
-REGEX = new RegExp(`([\\x${DEFAULT_ASCII_INDEX.toString(16)}-\\x${(DEFAULT_ASCII_INDEX + _.sum(DEFAULT_IMT_ARR)).toString(16)}]+)`, 'g');
-const T_INDEX = (DEFAULT_ASCII_INDEX + DEFAULT_IMT_ARR[0] + DEFAULT_IMT_ARR[1]);
 
-def encode():
+import re
+REGEX = re.compile('([{}-{}]+)'.format(hex(DEFAULT_ASCII_INDEX), hex(DEFAULT_ASCII_INDEX+sum(DEFAULT_IMT_ARR))))
+T_INDEX = (DEFAULT_ASCII_INDEX + DEFAULT_IMT_ARR[0] + DEFAULT_IMT_ARR[1])
 
-def decode():
+def init():
+    ascii_idx = DEFAULT_ASCII_INDEX
+    def mapping(s, d):
+        h2Ascii[s] = d
+        ascii2H[d] = s
+    for idx in range(DEFAULT_IMT_ARR[0]):
+        mapping((int('0x1100', 16) + idx), ascii_idx)
+        ascii_idx += 1
+    for idx in range(DEFAULT_IMT_ARR[1]):
+        mapping((int('0x1161', 16) + idx), ascii_idx)
+        ascii_idx += 1
+    for idx in range(DEFAULT_IMT_ARR[2]):
+        mapping((int('0x11A8', 16) + idx), ascii_idx)
+        ascii_idx += 1
+h2Ascii = {}, ascii2H = {}
+init()
+
+CONV_FUNC_TABLE = {
+    'conv_i_a2b': lambda c: int(((ord(c) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) / 21),
+    'conv_m_a2b': lambda c: int(((ord(c) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) % 21),
+    'conv_t_a2b': lambda c: int((ord(c) - KOREAN_UNICODE_DEFAULT_INDEX) % 28) - 1,
+
+    'conv_i_adv': lambda c: c + int('0x1100', 16),
+    'conv_m_adv': lambda c: c + int('0x1161', 16),
+    'conv_t_adv': lambda c: c + int('0x11A8', 16),
+
+    'conv_i_b2a': lambda i: (i - int('0x1100', 16)) * 588,
+    'conv_m_b2a': lambda i: (i - int('0x1161', 16)) * 28,
+    'conv_t_b2a': lambda i: (i - int('0x11A8', 16)) + 1,
+}
+
+def split_korean_char(char) {
+    return list((CONV_FUNC_TABLE.conv_i_a2b(char), CONV_FUNC_TABLE.conv_m_a2b(char), CONV_FUNC_TABLE.conv_t_a2b(char))
+        .filter(lambda c: c > -1, first))
+        .map(lambda (idx, c): (CONV_FUNC_TABLE.conv_i_adv, CONV_FUNC_TABLE.conv_m_adv, CONV_FUNC_TABLE.conv_t_adv)[idx](c))
+        .map(lambda c: h2Ascii[c])
+        .map(lambda c: chr(c)))
+}
+
+def merge_korean_char(chars) {
+    code_korean_char = KOREAN_UNICODE_DEFAULT_INDEX
+    code_korean_char += (CONV_FUNC_TABLE.conv_i_b2a, CONV_FUNC_TABLE.conv_m_b2a, CONV_FUNC_TABLE.conv_t_b2a)
+        .filter(lambda (idx, f): chars[idx])
+        .map(lambda (idx, f): f(ord(ascii2H[chars[idx]])))
+        .sum()
+    return chr(code_korean_char)
+}
+
+def encode(lines):
+    encoded_list = [split_korean_char(char) if LAST_KOREAN_CHAR >= char and char >= FIRST_KORAEN_CHAR else char for c in lines]
+    return str(encoded_list)
+
+def decode(lines):
+    def replace_func(p1):
+        new_str = []
+        l = len(p1)
+        i = 0
+        while i < l:
+            t_idx = (i + 2)
+            if t_idx < l and ord(p1[t_idx]) >= T_INDEX:
+                new_str.append(merge_korean_char(p1[i:i+3]))
+                i += 3
+            else:
+                new_str.append(merge_korean_char(p1[i:i+2]))
+                i += 2
+        return new_str
+    return REGEX.sub(replace_func, lines)
 
 def __main__(self):
     params
-
-
-#'use strict';
-
-# const _ = require('lodash');
-
-# module.exports = { option, encode, decode };
-
-# const FIRST_KORAEN_CHAR = '\uac00';
-# const LAST_KOREAN_CHAR = '\ud7af';
-# const KOREAN_UNICODE_DEFAULT_INDEX = parseInt('0xac00', 16);
-# ///////////////////////////////////////
-# function option(config) {
-#     if (config.using_charcode) {
-#         USING_CHARCODE = true;
-#         init();
-#     }
-# }
-# ///////////////////////////////////////
-# // 유니코드 한글은 0xAC00 으로부터 
-# // 초성 19개, 중상21개, 종성28개로 이루어지고
-# // 이들을 조합한 11,172개의 문자를 갖음
-# const DEFAULT_IMT_ARR = [19, 21, 28];
-# let USING_CHARCODE = false;
-# let DEFAULT_ASCII_INDEX = 128; // @WARN : '48' is not vouch to convert all of ascii code
-# const REGEX = new RegExp(`([\\x${DEFAULT_ASCII_INDEX.toString(16)}-\\x${(DEFAULT_ASCII_INDEX + _.sum(DEFAULT_IMT_ARR)).toString(16)}]+)`, 'g');
-# const T_INDEX = (DEFAULT_ASCII_INDEX + DEFAULT_IMT_ARR[0] + DEFAULT_IMT_ARR[1]);
-
-# let h2Ascii = {}, ascii2H = {};
-# function init() {
-#     let ascii_idx = DEFAULT_ASCII_INDEX;
-#     h2Ascii = {}, ascii2H = {};
-#     const mapping = (s, d) => { h2Ascii[s] = d; ascii2H[d] = s; }
-#     for (let i = 0, l = DEFAULT_IMT_ARR[0]; i < l; i++) mapping((parseInt('0x1100', 16) + i), (ascii_idx++))
-#     for (let i = 0, l = DEFAULT_IMT_ARR[1]; i < l; i++) mapping((parseInt('0x1161', 16) + i), (ascii_idx++))
-#     for (let i = 0, l = DEFAULT_IMT_ARR[2]; i < l; i++) mapping((parseInt('0x11A8', 16) + i), (ascii_idx++))
-# }
-# init();
-# ///////////////////////////////////////
-# const CONV_FUNC_TABLE = {
-#     conv_i_a2b: (c) => parseInt(((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) / 21),
-#     conv_m_a2b: (c) => parseInt(((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) % 21),
-#     conv_t_a2b: (c) => parseInt((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) % 28) - 1,
-
-#     conv_i_adv: (c) => c + parseInt('0x1100', 16),
-#     conv_m_adv: (c) => c + parseInt('0x1161', 16),
-#     conv_t_adv: (c) => c + parseInt('0x11A8', 16),
-
-#     conv_i_b2a: (i) => (i - parseInt('0x1100', 16)) * 588,
-#     conv_m_b2a: (i) => (i - parseInt('0x1161', 16)) * 28,
-#     conv_t_b2a: (i) => i - parseInt('0x11A8', 16) + 1,
-# };
-
-# function split_korean_char(char) {
-#     return _([
-#         CONV_FUNC_TABLE.conv_i_a2b(char),
-#         CONV_FUNC_TABLE.conv_m_a2b(char),
-#         CONV_FUNC_TABLE.conv_t_a2b(char)
-#     ]).filter(c => c > -1) // @INFO : valid check
-#         .map((c, idx) => [
-#             CONV_FUNC_TABLE.conv_i_adv,
-#             CONV_FUNC_TABLE.conv_m_adv,
-#             CONV_FUNC_TABLE.conv_t_adv
-#         ][idx](c))
-#         .map(c => h2Ascii[c])
-#         .map(c => String.fromCharCode(c))
-#         .value();
-# };
-
-# function merge_korean_char(chars) {
-#     let code_korean_char = KOREAN_UNICODE_DEFAULT_INDEX;
-#     _.map([
-#         CONV_FUNC_TABLE.conv_i_b2a,
-#         CONV_FUNC_TABLE.conv_m_b2a,
-#         CONV_FUNC_TABLE.conv_t_b2a,
-#     ], (f, idx) => {
-#         if (chars[idx]) code_korean_char += f.call(undefined, ascii2H[chars[idx].charCodeAt(0)]);
-#     })
-#     return String.fromCharCode(code_korean_char);
-# };
-# ///////////////////////////////////////
-# function encode(lines) {
-#     let encoded_list = _.flatten(_.map(lines, char => {
-#         // @INFO : we need to convert only korean chracter!
-#         if (LAST_KOREAN_CHAR >= char && char >= FIRST_KORAEN_CHAR) return split_korean_char(char);
-#         else return char;
-#     }));
-#     if (USING_CHARCODE) return _.map(encoded_list, c => c.charCodeAt(0));
-#     else return encoded_list.join('');
-# };
-
-# function decode(lines) {
-#     if (USING_CHARCODE) lines = _.map(lines, c => String.fromCharCode(c)).join('');
-#     return lines.replace(REGEX, (str, p1) => {
-#         let list = [];
-#         for (let i = 0, l = p1.length; i < l;) {
-#             const t_idx = (i + 2);
-#             if (t_idx < l && p1[t_idx].charCodeAt(0) >= T_INDEX) {
-#                 list.push(merge_korean_char(p1.substring(i, i += 3)));
-#             } else {
-#                 list.push(merge_korean_char(p1.substring(i, i += 2)));
-#             }
-#         }
-#         return list.join('');
-#     });
-# };
